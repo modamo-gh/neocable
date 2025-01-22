a80d445e6eab110b0fe488fd741643f5a62877d1
modamo-gh <morganthemosaic@gmail.com>
2025-01-21 23:28:07 -0500

    Show basic window

---
+++ src/main.ts
@@ -0,0 +1,22 @@
+import { app, BrowserWindow } from "electron";
+
+let mainWindow: BrowserWindow;
+
+app.on("ready", () => {
+	mainWindow = new BrowserWindow({
+		width: 800,
+		height: 600,
+		webPreferences: {
+			preload: `${__dirname}/preload.js`,
+			contextIsolation: true
+		}
+	});
+
+	mainWindow.loadFile("index.html");
+});
+
+app.on("window-all-closed", () => {
+	if (process.platform !== "darwin") {
+		app.quit();
+	}
+});
